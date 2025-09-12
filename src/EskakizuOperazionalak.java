f207627c9b039f1ce730f027494195a0dc06a295
Unai Navarro <irunavarro@lhusurbil.eus>
2025-09-12 13:10:19 +0200

    2.commita

--- src/EskakizuOperazionalak.java
+++ src/EskakizuOperazionalak.java
@@ -1,5 +1,6 @@
 public class EskakizuOperazionalak{
     public static void main (String[] args){
         System.out.println("Eskakizun operazionalak honako hauek dira eta azalduko dizkizut nire hitzekin");
+        System.out.println("1.Fidagarritasuna");
     }
 }
