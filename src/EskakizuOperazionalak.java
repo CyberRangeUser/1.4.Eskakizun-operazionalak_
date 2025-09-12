d2d2abb97953141b7d4bed15170c630e12b1a119
Unai Navarro <irunavarro@lhusurbil.eus>
2025-09-12 13:11:35 +0200

    3.commit Eskalagarritasuna hitza gehitzen

--- src/EskakizuOperazionalak.java
+++ src/EskakizuOperazionalak.java
@@ -2,5 +2,6 @@ public class EskakizuOperazionalak{
     public static void main (String[] args){
         System.out.println("Eskakizun operazionalak honako hauek dira eta azalduko dizkizut nire hitzekin");
         System.out.println("1.Fidagarritasuna");
+        System.out.println("2.Eskalagarritasuna");
     }
 }
