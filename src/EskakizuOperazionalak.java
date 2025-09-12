dd3d6883f054c9cbafdc9365972a0f3781b2d073
Unai Navarro <irunavarro@lhusurbil.eus>
2025-09-12 13:05:56 +0200

    lehenengo commit-a 1.4.EskakizunOperazionalekin

---
+++ src/EskakizuOperazionalak.java
@@ -0,0 +1,5 @@
+public class EskakizuOperazionalak{
+    public static void main (String[] args){
+        System.out.println("Eskakizun operazionalak honako hauek dira eta azalduko dizkizut nire hitzekin");
+    }
+}
